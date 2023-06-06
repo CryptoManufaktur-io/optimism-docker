aad762c071d933c66445214c79b064bc52f70c68
Yorick Downe <yorickdowne@gmail.com>
2023-06-06 17:27:03 +0000

    Download snapshot in /tmp

--- op-geth/docker-entrypoint.sh
+++ op-geth/docker-entrypoint.sh
@@ -39,6 +39,7 @@ esac
 # Prep datadir
 if [ ! -d "/var/lib/op-geth/geth/" ]; then
 #  wget -q -O - "${SNAPSHOT}" | tar xvf - -C /var/lib/op-geth
+  cd /tmp
   aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true
   filename=`echo ${SNAPSHOT} | awk -F/ '{print $NF}'`
   tar xvf ${filename} -C /var/lib/op-geth
